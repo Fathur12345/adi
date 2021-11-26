15ab6af9f5119fd3a0b3b15221dbe299e00218cf
Andriiwalker <93533006+Andriiwalker@users.noreply.github.com>
2021-11-26 11:00:03 +0700

    Update grupBot.js

--- plugins/grupBot.js
+++ plugins/grupBot.js
@@ -1,7 +1,7 @@
 let { MessageType } = require('@adiwajshing/baileys')
 let handler = async (m, { conn }) => {
 // JANGAN DI UBAH LAH BABI :V
-let grupBot = '*「 OFFICIAL BOT GROUP 」*\n\nSini Bang Join\n\n> http://bit.ly/Grupgabutbotv1'
+let grupBot = '*「 OFFICIAL BOT GROUP 」*\n\nSini Bang Join\n\n> https://chat.whatsapp.com/G2sqqakJTyl9KpFYAWzfEH'
 conn.reply(m.chat, grupBot, m)
 }
 handler.help = ['']
